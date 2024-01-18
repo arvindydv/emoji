c214770eb891ac1b7615edac081c5c9b34532d77
arvindydv <arvindydv03@gmail.com>
2024-01-18 14:56:01 +0530

    ok

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
  
  const emojeeContainer = document.querySelector(".emojee-container");
 
-document.addEventListener("DOMContentLoaded", ()=>{
+ const searchEmoji = ()=>{
+   const inputValue = document.getElementById("search").value;
+  //  console.log(inputValue);
+  displayData(inputValue);
+  return false;
+ }
 
-  emojiList.forEach((items)=>{
 
-    let emojiDiv = document.createElement("div");
-    emojiDiv.setAttribute("class", "emoji");
+  const displayData = (seacrhQuery="") =>{
 
-    emojiDiv.innerHTML = `
-    <h2> ${items.emoji} </h2>
-     <p class="cat"> ${items.category } </p>
-    <p class="des"> ${items.description } </p>
-   
-    `;
+    const filteredData = emojiList.filter((e)=>{
+       
+      if(e.description.indexOf(seacrhQuery) !== -1){
+        return true;
+      }
 
-    emojeeContainer.appendChild(emojiDiv);
-    
-  })
+      if(e.tags.some(e=>e.startsWith(seacrhQuery))){
+        return true;
+      }
+
+      if(e.aliases.some(e=>e.startsWith(seacrhQuery))){
+        return true;
+      }
+
+    })
+    emojeeContainer.innerHTML = ""
+    filteredData.forEach((items)=>{
+
+      let emojiDiv = document.createElement("div");
+      emojiDiv.setAttribute("class", "emoji");
   
+      emojiDiv.innerHTML = `
+      <h2> ${items.emoji} </h2>
+       <p class="cat"> ${items.category } </p>
+      <p class="des"> ${items.description } </p>
+     
+      `;
+  
+      emojeeContainer.appendChild(emojiDiv);
+  })
+
+}
+ 
+document.getElementById('search').addEventListener("keyup", searchEmoji);
+
+document.addEventListener('DOMContentLoaded',displayData());
+    
+ 
    
     
 
-
-})
